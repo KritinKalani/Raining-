5b317dcffe037e066de5a3e3ddbf4ddc7627fb1c
KritinKalani <kritinkalani@woodstock.ac.in>
2021-06-04 14:47:43 +0530

    Rain

--- Drop.js
+++ Drop.js
@@ -2,16 +2,20 @@ class Drop {
     constructor(x, y) {
         var options = {
             isStatic: false,
-            friction: 0.1
+            friction: 0.001,
+            restitution:0.1  
         }
         this.body = Bodies.circle(x, y, 10, options);
         this.image = loadImage("Drop.jpg")
         World.add(world, this.body);
     }
 
-    update() {
-        if (this.body.position.y > 500) {
-            Matter.Body.setPosition(this.body, { x: random(0, 500), y: random(0, 500) })
+    updateY() {
+        if (this.body.position.y > height) {
+             var dropx = Math.round(random(100, 450));
+            var dropy = Math.round(random(100, 450));
+            console.log("dropx: " + dropx + " dropy: " + dropy) 
+            Matter.Body.setPosition(this.body, {x:dropx, y:dropy})
         }
     }
 
--- sketch.js
+++ sketch.js
@@ -4,28 +4,63 @@ const Body = Matter.Body;
 const Engine = Matter.Engine;
 var dropArr = [];
 var maxDrops = 100;
+var random;
 
-function preload(){
-    
+function preload() {
+    lightning1 = loadImage('lightning1.png')
+    lightning2 = loadImage('lightning2.png')
+    lightning3 = loadImage('lightning3.png')
+    lightning4 = loadImage('lightning4.png')
+    lightning5 = loadImage('lightning5.png')
 }
 
-function setup(){
+function setup() {
+    createCanvas(500, 500);
     engine = Engine.create();
     world = engine.world;
-   createCanvas(500,500);
-   umbrella = new Umbrella(250,400);
-   for(var i = 0; i<maxDrops; i++){
-    dropArr.push(new Drop(random(0,500),random(0,500)));
-   }
+
+    umbrella = new Umbrella(250, 400);
+
+    for (var i = 0; i < maxDrops; i++) {
+       /*  var dropx = random(100, 500);
+        var dropy = random(100, 500);
+        console.log("dropx: " + dropx + " dropy: " + dropy) */
+        dropArr.push(new Drop(random(100, 500), random(100, 500)));
+    }
 }
 
-function draw(){
-    background(255,255,255);
-    Engine.update(engine)
+function draw() {
+    background(255, 255, 255);
+
+    Engine.update(engine);
+
     umbrella.display();
-    for(var i = 0; i<dropArr.length; i++){
+
+    for (var i = 0; i < maxDrops; i++) {
         dropArr[i].display();
-        dropArr[i].update();
+        dropArr[i].updateY();
     }
-}   
+
+    random = Math.round(Math.random(1, 5));
+
+    if (frameCount % 150 === 0) {
+        lightning = createSprite(Math.random(100, 400), Math.random(100, 400))
+        switch (random) {
+            case 1: lightning.addImage(lightning1)
+                break;
+            case 2: lightning.addImage(lightning2)
+                break;
+            case 3: lightning.addImage(lightning3)
+                break;
+            case 4: lightning.addImage(lightning4)
+                break;
+            case 5: lightning.addImage(lightning5)
+                break;
+            default: break;
+        }
+    }
+    if (frameCount % 162 === 0 && lightning) {
+        lightning.destroy();
+    }
+}
 
